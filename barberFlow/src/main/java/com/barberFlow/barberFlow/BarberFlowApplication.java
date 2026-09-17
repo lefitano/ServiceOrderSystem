11ca48b0e79d33764ae0688a667ece69127529e2
Leonardo Monteiro <leonardoms17@hotmail.com>
2026-09-17 09:37:01 -0300

    chore: estruturação inicial do projeto Spring Boot com a criação de pastas que serão utilizadas e Conexão com o MYSQL

---
+++ barberFlow/src/main/java/com/barberFlow/barberFlow/BarberFlowApplication.java
@@ -0,0 +1,13 @@
+package com.barberFlow.barberFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberFlowApplication.class, args);
+	}
+
+}
